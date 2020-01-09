9d25f1cd0040c160fc9d26ab04c236d6e63c2be1
amitverma07 <theverma7@gmail.com>
2020-01-09 11:15:56 -0600

    Another attempt to solve the git hub problem

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -11,7 +11,7 @@ class DBHelper {
     const port = 5500 // Change this to your server port
     //return `http://localhost:${port}/data/restaurants.json`;
     //return `https://amitverma07.github.io/Restaurant-Review-App/js/dbhelper.js`
-    return `https://github.com/amitverma07/Restaurant-Review-App/blob/master/data/restaurants.json`;
+    return `data/restaurants.json`;
   }
 
   /**
